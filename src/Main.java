73572ddedf153ce1bf381fc2fa2674be5b3a1a3f
VVZ 339 <vvzev@yandex.ru>
2023-05-03 09:26:47 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 24_000;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println(bonus);
+    }
+}
